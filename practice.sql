0ce366ec583aebf531411cd25d6844bf40e534d0
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-08-09 13:31:36 -0400

    practice 2

--- practice.sql
+++ practice.sql
@@ -2,3 +2,11 @@ select customer_id
 from customers
 where year = 2021 and revenue > 0
 group by customer_id
+
+
+select c.name as Customers
+from customers c 
+left join orders o 
+on c.id = o.customerID
+where c.id not in 
+(select customerid from orders)
