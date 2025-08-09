61b785742aa56f7f41a2bc627a95e497fb07b34c
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-08-09 14:26:49 -0400

    practice 3

--- practice.sql
+++ practice.sql
@@ -10,3 +10,23 @@ left join orders o
 on c.id = o.customerID
 where c.id not in 
 (select customerid from orders)
+
+
+select c.customer_id,
+    c.customer_name 
+from customers c
+left join orders o 
+on c.customer_id = o.customer_id 
+group by customer_id, customer_name
+having sum(o.product_name = 'A')> 0
+and sum(o.product_name= 'B')> 0
+and sum(o.product_name = 'C') = 0
+
+
+
+
+
+
+
+
+
