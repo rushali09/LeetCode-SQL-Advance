597bf0a3f469e530b481203ad20566594c29f61f
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-08-24 06:19:56 -0400

    store

--- practice.sql
+++ practice.sql
@@ -92,6 +92,27 @@ left join address a
 on p.personId = a.personId
 
 
+(select  product_id, 
+        'store1' as store, 
+        store1 as price
+from Products
+where store1 is not null)
+
+union all 
+
+(select  product_id,
+        'store2' as store,
+         store2 as price
+from products
+where store2 is not null)
+
+union all 
+
+(select product_id,
+        'store3' as store,
+        store3 as price
+from products
+where store3 is not null)
 
 
 
