39f3dde6a42b17dfa444cd335c8a739432ed4360
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-09-16 19:59:45 -0700

    Find the Team Size

---
+++ 1303.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Partition team_id into sections 
+# Which means the team id 8 is one group and 7 is another group and 9 is another group 
+# After partition into sections use count() window function
+
+select employee_id, count(employee_id) over(partition by team_id order by team_id)
+as team_size
+from employee
