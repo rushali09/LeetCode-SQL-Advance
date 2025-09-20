4a297a062b9937727d841cad07395a644aa30858
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-09-20 13:59:05 -0700

    Consecutive Available Seats

--- 603.sql
+++ 603.sql
@@ -1,6 +1,13 @@
-SELECT W. seat_id
-FROM (SELECT *, LEAD (free) OVER ORDER BY seat_id AS NextSeat, LAG free) OVER ORDER
-BY seat_id AS PrevSeat
-FROM Cinema AS W
-WHERE W. free = 1 AND W. NextSeat = 1
-ORDER BY W. seat_id
+-- # Write your MySQL query statement below
+-- # Keep only the seats that are free and the next or previous must be 1
+-- #If a seat is free and has at least one free neighbor → include it.
+
+select w.seat_id
+from 
+(select *,
+       lead(free)over(order by seat_id) as next_seat,
+       lag(free) over(order by seat_id) as prev_seat
+from cinema) as w
+where w.free= 1 and w.next_seat = 1 
+or w.free=1 and w.prev_seat= 1
+order by seat_id 
