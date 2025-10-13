b5ec5fb4f1ca621bcaf2cd7ab08784aa3a8f9e99
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-10-13 00:13:02 -0400

    The Most Frequently Ordered Products for Each Customer

---
+++ 1596.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+SELECT 
+    o.customer_id,
+    o.product_id,
+    p.product_name
+FROM (
+    SELECT 
+        customer_id,
+        product_id,
+        COUNT(*) AS order_count,
+        MAX(COUNT(*)) OVER (PARTITION BY customer_id) AS max_count
+    FROM Orders
+    GROUP BY customer_id, product_id
+) AS o
+JOIN Products p
+    ON o.product_id = p.product_id
+WHERE o.order_count = o.max_count;
+
+
+
+
+
